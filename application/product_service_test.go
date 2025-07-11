b82bafa6a58d784ea7bc13231764f7fccff01c6e
Vinicius Italo da Cruz <viniciusidacruz@hotmail.com>
2025-07-11 01:09:03 -0300

    feat: implemented and tested create method in product service

--- application/product_service_test.go
+++ application/product_service_test.go
@@ -24,3 +24,19 @@ func TestProductService_Get(t *testing.T) {
 	require.Nil(t, err)
 	require.Equal(t, product, productResult)
 }
+
+func TestProductService_Create(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	product := mock_application.NewMockProductInterface(ctrl)
+	persistence := mock_application.NewMockProductPersistenceInterface(ctrl)
+
+	persistence.EXPECT().Save(gomock.Any()).Return(product, nil).AnyTimes()
+
+	service := application.ProductService{Persistence: persistence}
+
+	productResult, err := service.Create("Product 1", 10.0)
+	require.Nil(t, err)
+	require.Equal(t, product, productResult)
+}
