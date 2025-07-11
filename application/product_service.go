be67fc840b28869967e606f034473eed58a7270a
Vinicius Italo da Cruz <viniciusidacruz@hotmail.com>
2025-07-11 01:17:01 -0300

    feat: implemented and tested enable/disable methods in product service

--- application/product_service.go
+++ application/product_service.go
@@ -37,3 +37,35 @@ func (s *ProductService) Create(name string, price float64) (ProductInterface, e
 
 	return result, nil
 }
+
+func (s *ProductService) Enable(product ProductInterface) (ProductInterface, error) {
+	err := product.Enable()
+
+	if err != nil {
+		return &Product{}, err
+	}
+
+	result, err := s.Persistence.Save(product)
+
+	if err != nil {
+		return &Product{}, err
+	}
+
+	return result, nil
+}
+
+func (s *ProductService) Disable(product ProductInterface) (ProductInterface, error) {
+	err := product.Disable()
+
+	if err != nil {
+		return &Product{}, err
+	}
+
+	result, err := s.Persistence.Save(product)
+
+	if err != nil {
+		return &Product{}, err
+	}
+
+	return result, nil
+}
--- application/product_service_test.go
+++ application/product_service_test.go
@@ -40,3 +40,37 @@ func TestProductService_Create(t *testing.T) {
 	require.Nil(t, err)
 	require.Equal(t, product, productResult)
 }
+
+func TestProductService_Enable(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	product := mock_application.NewMockProductInterface(ctrl)
+	persistence := mock_application.NewMockProductPersistenceInterface(ctrl)
+
+	product.EXPECT().Enable().Return(nil).AnyTimes()
+	persistence.EXPECT().Save(gomock.Any()).Return(product, nil).AnyTimes()
+
+	service := application.ProductService{Persistence: persistence}
+
+	productResult, err := service.Enable(product)
+	require.Nil(t, err)
+	require.Equal(t, product, productResult)
+}
+
+func TestProductService_Disable(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	product := mock_application.NewMockProductInterface(ctrl)
+	persistence := mock_application.NewMockProductPersistenceInterface(ctrl)
+
+	product.EXPECT().Disable().Return(nil).AnyTimes()
+	persistence.EXPECT().Save(gomock.Any()).Return(product, nil).AnyTimes()
+
+	service := application.ProductService{Persistence: persistence}
+
+	productResult, err := service.Disable(product)
+	require.Nil(t, err)
+	require.Equal(t, product, productResult)
+}
