cd1169b3b17cfdee04c0d6d343bf385c5311b78e
Vinicius Italo da Cruz <viniciusidacruz@hotmail.com>
2025-07-12 01:35:04 -0300

    feat: implement save method in product adapter with tests

--- adapters/db/product.go
+++ adapters/db/product.go
@@ -33,3 +33,43 @@ func (p *ProductDb) Get(id string) (application.ProductInterface, error) {
 
 	return &product, nil
 }
+
+func (p *ProductDb) create(product application.ProductInterface) (application.ProductInterface, error) {
+	stmt, err := p.db.Prepare("INSERT INTO products (id, name, price, status) VALUES (?, ?, ?, ?)")
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer stmt.Close()
+
+	_, err = stmt.Exec(product.GetID(), product.GetName(), product.GetPrice(), product.GetStatus())
+
+	if err != nil {
+		return nil, err
+	}
+
+	return product, nil
+}
+
+func (p *ProductDb) update(product application.ProductInterface) (application.ProductInterface, error) {
+	_, err := p.db.Exec("UPDATE products SET name = ?, price = ?, status = ? WHERE id = ?", product.GetName(), product.GetPrice(), product.GetStatus(), product.GetID())
+
+	if err != nil {
+		return nil, err
+	}
+
+	return product, nil
+}
+
+func (p *ProductDb) Save(product application.ProductInterface) (application.ProductInterface, error) {
+	var rows int
+
+	p.db.QueryRow("SELECT count(*) FROM products WHERE id = ?", product.GetID()).Scan(&rows)
+
+	if rows == 0 {
+		return p.create(product)
+	}
+
+	return p.update(product)
+}
--- adapters/db/product_test.go
+++ adapters/db/product_test.go
@@ -7,19 +7,20 @@ import (
 
 	_ "github.com/mattn/go-sqlite3"
 	"github.com/stretchr/testify/require"
+	"github.com/viniciusidacruz/hexagonal-archtecture/application"
 )
 
 var Db *sql.DB
 
 func Setup() {
-    var err error
-    Db, err = sql.Open("sqlite3", ":memory:")
-    if err != nil {
-        log.Fatal(err)
-    }
+	var err error
+	Db, err = sql.Open("sqlite3", ":memory:")
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    createTable(Db)
-    createProduct(Db)
+	createTable(Db)
+	createProduct(Db)
 }
 
 func createTable(db *sql.DB) {
@@ -68,4 +69,34 @@ func TestProductDb_Get(t *testing.T) {
 	require.Equal(t, "Product 1", product.GetName())
 	require.Equal(t, 100.00, product.GetPrice())
 	require.Equal(t, "enabled", product.GetStatus())
+}
+
+func TestProductDb_Save(t *testing.T) {
+	Setup()
+
+	defer Db.Close()
+
+	productDb := NewProductDb(Db)
+
+	product := application.NewProduct()
+	product.Name = "Product 2"
+	product.Price = 200
+
+	productCreated, err := productDb.Save(product)
+
+	require.Nil(t, err)
+	require.Equal(t, product.GetID(), productCreated.GetID())
+	require.Equal(t, product.GetName(), productCreated.GetName())
+	require.Equal(t, product.GetPrice(), productCreated.GetPrice())
+	require.Equal(t, product.GetStatus(), productCreated.GetStatus())
+
+	product.Status = "enabled"
+
+	productUpdated, err := productDb.Save(product)
+
+	require.Nil(t, err)
+	require.Equal(t, product.GetID(), productUpdated.GetID())
+	require.Equal(t, product.GetName(), productUpdated.GetName())
+	require.Equal(t, product.GetPrice(), productUpdated.GetPrice())
+	require.Equal(t, product.GetStatus(), productUpdated.GetStatus())
 }
